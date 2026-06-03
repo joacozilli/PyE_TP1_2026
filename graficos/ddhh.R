0a0e6bdbac4f82d295819955275ec628475fa854
Alan Hergenreder <alan.hergen2255@gmail.com>
2026-06-03 19:45:18 -0300

    Agrego grafico de cantidad de paises segun ddhh

---
+++ graficos/ddhh.R
@@ -0,0 +1,24 @@
+library(googlesheets4)
+library(ggplot2)
+library(dplyr)
+
+# 
+url <- "https://docs.google.com/spreadsheets/d/1Kwl4KByOv8q2kXMsgaO3d5QI3vUQ40RCZJgJHhg5bmE/edit#gid=580479207"
+
+# Acceso público
+gs4_deauth()
+
+# LEER DATOS 
+datos <- read_sheet(url,sheet = "tp", skip = 1)
+
+#---------------------------------------------------------------
+#Grafico para ddhh 
+# Analisis univariado de una continua.
+ggplot(datos, aes(x = ddhh)) +
+  geom_histogram(bins = 15, fill = "lightblue", color = "white") +
+  labs(title = "Distribución del países según su puntaje de Derechos Humanos en IA",
+       x = "Puntaje ddhh (0-100)",
+       y = "Cantidad de países") +
+  theme_minimal()
+#
+
